a825340767bbfaf20f96bb41fa168df66c083497
Prowly <prowly@tuta.io>
2022-02-03 00:49:50 +0000

    OwO

--- helloworld.c
+++ helloworld.c
@@ -21,4 +21,31 @@ Purpose: There is no purpose.
     
 // }
 
----------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+
+// int main()
+
+// {
+    
+//     double width = 69.8;
+//     double height = 90.4;
+//     double perimeter = 0.0;
+//     double area = 0.0;
+    
+//     perimeter = 2.0 * (height + width);
+//     area = width * height;
+    
+//     printf("\n");
+//     printf("Width is: %.2f, the Height is: %.2f\n\n", width , height);
+//     printf("Perimeter is: %.2f\n\n", perimeter);
+//     printf("Area is: %.2f\n\n", area);
+//     printf("És um conas Peixoto!!\n\n");
+    
+//     return 0;
+    
+// }

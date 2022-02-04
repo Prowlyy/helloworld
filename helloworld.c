278263c4900f0c72dc651c913b070989843400b1
Prowly <prowly@tuta.io>
2022-02-04 15:53:19 +0000

    o

--- helloworld.c
+++ helloworld.c
@@ -75,3 +75,22 @@ Purpose: There is no purpose.
 // }
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a = 33;
+    int b = 15;
+    int result = 0;
+    result = a + b;
+
+    printf("Result is %d\n", result);
+
+    return 0;
+
+}
+
+
+

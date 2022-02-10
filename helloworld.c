d3d3c45dfa2b8da38b933eacbbc8405bedfb1afd
Prowly <prowly@tuta.io>
2022-02-10 19:01:50 +0000

    Owo

--- helloworld.c
+++ helloworld.c
@@ -1,96 +1,85 @@
-/*
-Author: PRRRRRRRRRRRRRRRRRRRRRRRRR
-Purpose: There is no purpose.
-*/
-
-
-// #include <stdio.h>
-
-// int main()
-// {
-//     char str[100];
-//     int i;
-
-//     printf("Etner a value:");
-
-//     scanf("%d", &i);
-
-//     printf("\nYou entered: %d\n", i);
-
-//     return 0;
-    
-// }
-
-// ---------------------------------------------------------------------------------------------------
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-
-// int main()
-
-// {
-    
-//     double width = 69.8;
-//     double height = 90.4;
-//     double perimeter = 0.0;
-//     double area = 0.0;
-    
-//     perimeter = 2.0 * (height + width);
-//     area = width * height;
-    
-//     printf("\n");
-//     printf("Width is: %.2f, the Height is: %.2f\n\n", width , height);
-//     printf("Perimeter is: %.2f\n\n", perimeter);
-//     printf("Area is: %.2f\n\n", area);
-//     printf("És um conas Peixoto!!\n\n");
-    
-//     return 0;
-    
-// }
-// ---------------------------------------------------------------------------------------------------
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// int main()
-// {
-    
-//     enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT };
-  
-//     enum Company Xerox = XEROX;
-//     enum Company google = GOOGLE;
-//     enum Company ebay = EBAY;
-    
-//     printf("The value of Xerox is: %d\n", Xerox);
-//     printf("The value of Google is: %d\n", google);
-//     printf("The value of Ebay is: %d\n", ebay);
-
-   
-   
-   
-//     return 0;
-// }
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PAYRATE 12.00
+#define TAXRATE_300 .15
+#define TAXRATE_150 .20
+#define TAXRATE_REST .25
+#define OVERTIME 40
+
+
+
 int main()
 {
+ //declare variables
+ int hours = 0;
+ double grossPay = 0.0;
+ double taxes = 0.0;
+ double netPay = 0.0;
 
-    int a = 33;
-    int b = 15;
-    int result = 0;
-    result = a + b;
+ //Get hours from user
+ printf("Please enter the number of hours you worked this week:\n");
+ scanf("%d", &hours);
 
-    printf("Result is %d\n", result);
+ // Calculate hours worked in week with payrate
 
-    return 0;
+ if (hours <= 40)
+     grossPay = hours * PAYRATE;
+ else
+
+ {
+     grossPay = 40 * PAYRATE;
+     double overTimePay = (hours - 40) * (PAYRATE * 1.5);
+     grossPay = grossPay + overTimePay;
+ }
+
+//cALCULATE TAXES
+
+ if (grossPay <= 300)
+ {
+     taxes = grossPay * TAXRATE_300;
+ }
+ else if (grossPay > 300 && grossPay < 450)
+ {
+     taxes = 300 * TAXRATE_300;
+     taxes += (grossPay - 300) * TAXRATE_150;
+ }
+ else if (grossPay > 450)
+ {
+     taxes = 300 * TAXRATE_300;
+     taxes += 150 * TAXRATE_150;
+     taxes += (grossPay - 450) * TAXRATE_REST;
+ }
+
+
+//Calculate net pay
+
+ netPay = grossPay - taxes;
+
+ //Display output
+
+ printf("Your gross pay this week is: %.2f\n", grossPay);
+ printf("Your taxes this week is: %.2f\n", taxes);
+ printf("Your net pay this week is: %.2f\n", netPay);
+
+
+
+
+
+
+
+// if(hoursWorked > 40)
+//    basicPayRate = 18.00;
+//    printf("This is how much you receive in over time pay %d:\n", basicPayRate * hoursWorked > 40);
+
+
+
+
+
+
+
+
+
+ return 0;
 
 }
-
-
-

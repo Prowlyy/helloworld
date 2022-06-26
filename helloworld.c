d68a2631678c265ebd0218d7d99bb2d2846cbe8e
Prowly <prowly@tuta.io>
2022-06-26 20:09:19 +0100

    asdasda

--- helloworld.c
+++ helloworld.c
@@ -2,23 +2,94 @@
 #include <stdlib.h>
 
 
+int gcd(int u, int v);
+float absoluteValue(float x);
+float squareRoot(float x);
 
-void multiplyTwoNumbers (int x, int y)
 
-{
-    int result = x * y;
-    printf("The product of %d multiplied by %d is: %d\n", x, y, result);
-}
 
-int main (void)
+int main ()
 
 {
+    int result = 0;
 
-    multiplyTwoNumbers (10,20);
-    multiplyTwoNumbers (20,30);
-    multiplyTwoNumbers (50,2);
+    float f1 = -15.5, f2 = 20.0, f3= -5.0;
+    int i1 = 716;
+    float absoluteValueResult = 0.0;
 
+
+
+    result = gcd(150, 35);
+    printf("The gcd of 150 and 35 is %d\n", result);
+
+
+    result = gcd(255, 35);
+    printf("The gcd of 150 and 35 is %d\n", result);
+
+    result = gcd(3333, 35);
+    printf("The gcd of 150 and 35 is %d\n", result);
+
+    result = gcd(512, 35);
+    printf("The gcd of 150 and 35 is %d\n", result);
+
+    result = gcd(152, 35);
+    printf("The gcd of 150 and 35 is %d\n", result);
+
+
+    /*testing aboslute value function */    
+    absoluteValueResult = absoluteValue (f1);
+    printf ("result = %.2f\n", absoluteValueResult);
+    printf ("f1 = %.2f\n", f1);
+
+    absoluteValueResult = absoluteValue (f2) + absoluteValue (f3);
+    printf ("result = %.2f\n", absoluteValueResult);
+
+    absoluteValueResult = absoluteValue ( (float) i1 );
+    printf ("result = %.2f\n", absoluteValueResult);
+
+    absoluteValueResult = absoluteValue (i1);
+    printf ("result = %.2f\n", absoluteValueResult);
+
+    printf ("%.2f\n\n\n\n", absoluteValue (-6.0) / 4 );
     return 0;
 
+    
+    printf("%.2f\n", squareRoot(-3.0));
+    printf("%.2f\n", squareRoot(16.0));
+    printf("%.2f\n", squareRoot(25.0));
+    printf("%.2f\n", squareRoot(9.0));
+    printf("%.2f\n", squareRoot(225.0));
 
+
+}
+
+
+float squareRoot(float x)
+{
+    const  float  epsilon = .00001;
+    float  guess   = 1.0;
+    float returnValue = 0.0;
+
+    if ( x < 0 )
+    {
+        printf ("Negative argument to squareRoot.\n");
+        returnValue = -1.0;
+    }
+    else
+    {
+        while  ( absoluteValue (guess * guess - x) >= epsilon )
+           guess = ( x / guess + guess ) / 2.0;
+
+        returnValue = guess;
+    }
+
+    return returnValue;
+}
+
+float absoluteValue(float x)
+{
+    if (x < 0)
+        x = -x;
+
+    return x;
 }
